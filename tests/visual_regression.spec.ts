0a0ab3458acf924297f914c67b3f8a5762b61c58
jozefkepinski <jozefkepinski@hotmail.com>
2024-03-27 21:07:21 +0100

    added Promise only for toHaveScreenshot

--- tests/visual_regression.spec.ts
+++ tests/visual_regression.spec.ts
@@ -3,10 +3,10 @@ import { test, expect } from '@playwright/test';
 import path from 'path';
 
 test('Strona główna wygląda poprawnie', async ({ page }) => {
+    await page.goto('https://www.24mx.pl/', {waitUntil: 'domcontentloaded'}),
+    await page.waitForSelector('//*[@id="wrapper"]/div/p-home/div[1]/div[1]/p-cms-dynamic-renderer[9]/p-cms-container/div/div/div/p-cms-dynamic-renderer/p-cms-article-blurb/a/div[1]'),
+    await page.waitForTimeout(2000),
     await Promise.all([
-        await page.goto('https://www.24mx.pl/', {waitUntil: 'domcontentloaded'}),
-        await page.waitForSelector('//*[@id="wrapper"]/div/p-home/div[1]/div[1]/p-cms-dynamic-renderer[9]/p-cms-container/div/div/div/p-cms-dynamic-renderer/p-cms-article-blurb/a/div[1]'),
-        await page.waitForTimeout(2000),
         expect(page).toHaveScreenshot('/screenshots/HomePage.png')
     ])
 });
@@ -21,8 +21,8 @@ test('Wyszukiwanie produktu', async ({ page }) => {
     await page.fill('#search-desktop', 'kask motocross');
     await page.waitForTimeout(1000);
     await page.click("(//a[@class='autocomplete-item'])[1]");
+    await page.waitForTimeout(2000)
     await Promise.all([
-        await page.waitForTimeout(2000),
         expect(page).toHaveScreenshot('/screenshots/SearchResults.png', {clip: {x:0, y:0, height:282, width:1280}})
     ])
 });
@@ -33,18 +33,19 @@ test('Dodawanie produktu do koszyka', async ({ page }) => {
         await page.waitForSelector('//*[@id="wrapper"]/div/p-home/div[1]/div[1]/p-cms-dynamic-renderer[9]/p-cms-container/div/div/div/p-cms-dynamic-renderer/p-cms-article-blurb/a/div[1]'),
         await page.click("//a[@class='m-navigation-link'][normalize-space()='Kaski']"),
     ])
+    await page.waitForTimeout(1500)
+    await page.screenshot({path:'tests/screenshots/' + 'debug.png'})
     await page.click("(//img[@alt='Kask Cross'])[1]");
+    // await page.locator("(//img[@alt='Kask Cross'])[1]").click()
     await page.click("div[class='m-product-card-img'] img[title='Kask Cross Raven Airborne Evo Czarny']");
     await page.click("//div[@class='m-select__display']");
     await page.click("(//div[@class='a-product-variation'])[3]");
     await page.click(".m-button.m-button--purchase.qa-pdp-add-to-cart-btn.m-button--md");
+    await page.click("//a[contains(text(),'Przejdź do kasy')]"),
+    await page.waitForLoadState('domcontentloaded')
+    await page.waitForSelector("//div[@class='m-checkout-box m-checkout-box--delivery']//div[@class='m-checkout-box__heading']")
+    await page.waitForTimeout(2000)
     await Promise.all([
-        await page.click("//a[contains(text(),'Przejdź do kasy')]"),
-        await page.waitForLoadState('domcontentloaded')
-    ])
-    await Promise.all([
-        await page.waitForSelector("//div[@class='m-checkout-box m-checkout-box--delivery']//div[@class='m-checkout-box__heading']"),
-        await page.waitForTimeout(2000),
         expect(page).toHaveScreenshot('/screenshots/koszyk.png', {clip: {x:0, y:0, height:224, width:514}})
     ])
 });
